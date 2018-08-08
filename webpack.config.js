ebddf492e79248804243babf9628422c4010359b
Sirbu Nicolae-Cezar <sirbunicolaecezar@gmail.com>
2018-08-08 19:22:55 +0300

    Add bootstrap

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const webpack = require('webpack');
 
 const output = 'static';
 
@@ -18,10 +19,24 @@ module.exports = {
           use: ['css-loader', 'less-loader'],
           fallback: 'style-loader'
         })
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'fonts/'
+          }
+        }]
       }
     ]
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    }),
     new HtmlWebpackPlugin({
       template: './src/index.html'
     }),
